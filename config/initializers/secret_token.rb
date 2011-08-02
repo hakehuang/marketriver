3a74ab4c5f024da0c2228be5dc3ef0735e9b4017
Hake Huang <b20222@freescale.com>
2011-08-02 22:08:26 +0800

    initial an almost empty git
    Signed-off-by: Hake Huang <b20222@freescale.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phoenix::Application.config.secret_token = '6a167ca46f57108dd77e0f3d606b5f662210b54515a79094397cb6b96f108d46bafc1f84cfbbcd7a478ed7e8fd73857690f39d6d192774d2d7e745da8a938505'
