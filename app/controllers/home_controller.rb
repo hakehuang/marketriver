dc5f239f349ab7252e6398e8868f1f3a046e5c81
Hake Huang <b20222@freescale.com>
2011-10-02 22:38:30 +0800

    reszie a bit and add random products display but remain some hot and pop pos
    Signed-off-by: Hake Huang <b20222@freescale.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-  end
+     @products = Product.all(:order => "RAND()", :limit => 5)
 
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @products }
+    end
+
+  end
 end
