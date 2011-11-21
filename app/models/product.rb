adcd992f48da41226034443f7275c8d9617904d7
Hake Huang <hakehuang@gmail.com>
2011-11-21 22:37:03 +0800

    add dependency of transaction
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :DataFiles, :dependent => :destroy
-  has_many :Transactions
+  has_many :Transactions, :dependent => :destroy
   validates :cata_level_1,  :presence => true
   validates :cata_level_2,  :presence => true
 end
