8653d9b7d6ea1669c8ca50f619361414303c3d51
Hake Huang <hakehuang@gmail.com>
2011-12-10 11:40:34 +0800

    add mail nogify for devise
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,8 @@ Phoenix::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:80' }
+
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.smtp_settings = {
   :address              => "mail.enjoybee.com",
