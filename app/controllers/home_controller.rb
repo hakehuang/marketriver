275fcd750905e006092a5eefd6107c3b0b1edc41
Hake Huang <hakehuang@gmail.com>
2012-01-01 22:46:45 +0800

    prompt 15 products
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     @product_types.each do |pd|
     @products_div[pd.id]  = Product.where("cata_level_1 = :type", :type => pd.id ).all(:order => "RAND()", :limit => 5)
     end
-    @producs_prompt = Product.where("prompt = :pr", :pr => true).all(:order => "RAND()", :limit => 5)
+    @producs_prompt = Product.where("prompt = :pr", :pr => true).all(:order => "RAND()", :limit => 15)
     #only keep code here
     #current_user.update_attribute :admin, true
 
