43e23c1a4e7280bb984eab6e5acb066c7cda4b47
Hake Huang <hakehuang@gmail.com>
2012-06-24 22:56:54 +0800

    enalbe local mail for test
    Signed-off-by: Hake Huang <hakehuang@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Phoenix::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  #config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
